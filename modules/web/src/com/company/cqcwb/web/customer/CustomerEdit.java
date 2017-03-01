26100f363b9df91e24843d830ee0918b151f1d41
Mario David <mariodavid@arcor.de>
2017-03-01 12:33:44 +0100

    initial UI screens for customer

---
+++ modules/web/src/com/company/cqcwb/web/customer/CustomerEdit.java
@@ -0,0 +1,7 @@
+package com.company.cqcwb.web.customer;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.company.cqcwb.entity.Customer;
+
+public class CustomerEdit extends AbstractEditor<Customer> {
+}
